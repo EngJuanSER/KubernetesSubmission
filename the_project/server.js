9ecf983a934972f9b5ccd797ab0baa677f5e6ce2
EngJuanSER <jumserranor@udistrital.edu.co>
2025-12-09 11:05:45 -0500

    Exercise 1.5: Add HTTP endpoint to todo-app

--- the_project/server.js
+++ the_project/server.js
@@ -3,8 +3,13 @@ const http = require('http');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Todo App!\n');
+  if (req.url === '/' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Todo App</h1><p>Welcome to the todo application!</p>');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
 });
 
 server.listen(PORT, () => {
