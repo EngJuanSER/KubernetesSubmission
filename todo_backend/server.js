c4454c5cbe31d7c5b3f6de68b9606bab69f8698a
EngJuanSER <jumserranor@udistrital.edu.co>
2025-12-21 19:35:22 -0500

    Test: Trigger staging deployment with separate repo

--- todo_backend/server.js
+++ todo_backend/server.js
@@ -3,6 +3,7 @@ const url = require('url');
 const { Pool } = require('pg');
 const { connect, StringCodec } = require('nats');
 
+// Exercise 4.10: Using separate repositories for code and config
 const PORT = process.env.PORT || 3000;
 const NATS_URL = process.env.NATS_URL || 'nats://my-nats.project.svc.cluster.local:4222';
 
